0c8a48ccf9eba2c29147acd00f5d9b4dadf5e0bd
Max <teratoscincus@hotmail.com>
2023-01-23 10:26:36 +0100

    Fix settings so player jumps over obstacle
    Obstacle was perviously to slow so the player would always land on in.

--- settings.py
+++ settings.py
@@ -3,15 +3,15 @@ import pygame
 
 class Settings:
     def __init__(self) -> None:
-        self.screen_width = 700
-        self.screen_height = 400
+        self.screen_width = 1200
+        self.screen_height = 800
         self.bg_color = (60, 0, 80)
 
-        self.player_gravity = 0.1
+        self.player_gravity = 0.075
         self.player_speed = 0
-        self.player_jump_height = -6
+        self.player_jump_height = -7.75
 
-        self.obstacle_speed = 1  # Måste vara heltal annars kan inte x bli lägre än 0.
+        self.obstacle_speed = 1.5  # Måste vara över 1 annars kan inte x bli lägre än 0.
         self.obstacle_respawn_rate = (
             -500
         )  # Hur långt utanför vänsterkant innan ny kommer.
