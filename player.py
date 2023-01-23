1429021895ef3516f88542e0207011db81104275
Max <teratoscincus@hotmail.com>
2023-01-23 11:32:19 +0100

    Fix Player docs

--- player.py
+++ player.py
@@ -4,33 +4,38 @@ from settings import Settings
 
 
 class Player:
-    def __init__(self) -> None:
+    def __init__(self):
+        """Instansiera spelaren och dennes förutsättningar."""
         self.settings = Settings()
         self.gravity = self.settings.player_gravity
         self.speed = self.settings.player_speed
         self.jump_height = self.settings.player_jump_height
-        self.is_jumping = False
+        self.is_jumping = False  # Flaggvariabel för att påbörja ett hopp.
         self.step = 0  # Används för att alternera image_running1 och image_running2.
 
+        # Spelplanens gränser.
         self.screen = self.settings.screen
         self.screen_rect = self.settings.screen_rect
 
+        # Ladda spelarens bilder.
         self.image_idle = pygame.image.load("images/adventurer_idle.png")
         self.image_jumping = pygame.image.load("images/adventurer_jump.png")
         self.image_running1 = pygame.image.load("images/adventurer_action1.png")
         self.image_running2 = pygame.image.load("images/adventurer_action2.png")
 
         self.image = self.image_idle
-        self.rect = self.image.get_rect()
+        self.rect = self.image.get_rect()  # Mät spelarens gränser.
 
-        # Placera spelarens nedre vänsterkant diktan med spelskärmens nedre vänsterkant.
-        self.rect.bottomleft = self.screen_rect.bottomleft
+        self._spawn()
 
     def blitme(self):
+        """Ritar sig på skärmen."""
         self.screen.blit(self.image, self.rect)
 
     def update(self):
+        """Updaterar sin position."""
         self.step += 1
+
         if self.is_jumping:
             self.image = self.image_jumping
 
@@ -47,6 +52,18 @@ class Player:
             else:
                 self.image = self.image_running2
 
+        # Uppdatera spelarens gränser, i det fall att spelaren hoppar eller duckar.
+        self.rect = self.image.get_rect()
+
     def jump(self):
+        """Spelaren innleder ett hopp."""
         self.speed = self.jump_height
         self.is_jumping = True
+
+    def _spawn(self):
+        """
+        Placera ut spelaren på spelskärmen.
+
+        Spelarens nedre vänsterkant placeras diktan med spelskärmens nedre vänsterkant.
+        """
+        self.rect.bottomleft = self.screen_rect.bottomleft
