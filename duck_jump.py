38a9b46cd3071fd3cde2727d474262fb028bcbaf
Max <teratoscincus@hotmail.com>
2023-01-23 11:45:59 +0100

    Fixa documentation i DuckAndJump

--- duck_jump.py
+++ duck_jump.py
@@ -8,44 +8,60 @@ from obstacle import Obstacle
 
 
 class DuckAndJump:
-    def __init__(self) -> None:
+    def __init__(self):
+        """
+        Instansiera spelet med inställningar, skärm, spelaren, och hinder attribut.
+        """
+        # Starta pygame.
         pygame.init()
 
+        # Spelets inställningar och skärm.
         self.settings = Settings()
         self.screen = self.settings.screen
+        pygame.display.set_caption("Duck and Jump")  # Titel på spelets fönster.
 
-        pygame.display.set_caption("Duck and Jump")
-
+        # Spelets spelare och hinder.
         self.player = Player()
         self.obstacle = Obstacle()
 
     def run_game(self):
+        """Spelets körning."""
         while True:
+            # Kolla efter input från användaren.
             self._check_events()
-            self.obstacle.update()
+
+            # Uppdatera spelaren och hindren.
             self.player.update()
+            self.obstacle.update()
+
+            # Uppdatera skärmen.
             self._update_screen()
 
     def _update_screen(self):
-        """Updaterar skärmen of 'flippar' till en ny skärm."""
+        """Updaterar skärmen of 'flippar' till en ny bild."""
+        # Rita spelets bakgrund och element.
         self.screen.fill(self.settings.bg_color)
-        self.obstacle.blitme()
         self.player.blitme()
+        self.obstacle.blitme()
 
+        # 'Flippa' till en ny bild.
         pygame.display.flip()
 
     def _check_events(self):
         """Kolla efter tangentbords- och mushändelser."""
         for event in pygame.event.get():
+            # Klickar kryssrutan eller anvönder genväg för att stänga fönster.
             if event.type == pygame.QUIT:
                 sys.exit()
 
-            # Styrning av spelaren.
+            # Nedtryckning av tangentbordsknappar.
             elif event.type == pygame.KEYDOWN:
+                # Styrning av spelaren.
                 if event.key == pygame.K_UP:
                     self.player.jump()
 
 
 if __name__ == "__main__":
+    # Instansiera spelet och kalla på dess metod för att starta spelets loop.
     game = DuckAndJump()
     game.run_game()
