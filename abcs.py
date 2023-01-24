2622bf9f2c4ccd457d90cd8ccb92b77d9372d155
Max <teratoscincus@hotmail.com>
2023-01-24 19:07:52 +0100

    Skapa abstrakt basklass

---
+++ abcs.py
@@ -0,0 +1,45 @@
+"""En modul för abstrakta classer."""
+
+from settings import Settings
+
+
+class AbstractBaseGameElement:
+    """
+    En abstract basklass.
+
+    Används för att reducera kodduplicering genom att låta andra klasser för spelelement
+    ärva från denna klass.
+    """
+
+    def __init__(self):
+        """
+        Initierar attribut vanliga för spelelement.
+
+        Användning:
+            Initiera efter att ett "image" attribut har deklarerats med
+            "pygame.image.load()" som värde. Detta då attributet "rect" får sitt värde
+            från "image" attributets ".get_rect()" metod.
+        """
+        self.settings = Settings()
+        # Spelplanens gränser.
+        self.screen = self.settings.screen
+        self.screen_rect = self.settings.screen_rect
+
+        # Mät spelarens utkant, och skapa en 'hitbox' som är mindre än utkanten.
+        self.rect = self.image.get_rect()
+        hitbox_width = self.rect.width / 2
+        hitbox_height = self.rect.height / 2
+        self.hitbox = self.rect.inflate(-hitbox_width, -hitbox_height)
+
+    def blitme(self):
+        """Ritar sig på skärmen."""
+        self.screen.blit(self.image, self.rect)
+
+    def update(self):
+        """
+        Centrera 'hitbox'.
+
+        Användning:
+            Används som förlängning av kod specifik för den ärvande klassen.
+        """
+        self.hitbox.center = self.rect.center
